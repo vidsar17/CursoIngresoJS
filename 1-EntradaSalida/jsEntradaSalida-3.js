4d269101a48b6b4ee91cf84107b456a35680f565
alumno <alumno@utnfra.com.ar>
2020-02-04 19:09:56 -0300

    Ejercicio 3 sin terminar

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,12 @@
 /*Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
-function mostrar()
+function mostrar(dato)
 {
-	
+	var dato;
+
+    dato = Document.getElementById('elNombre');
+
+    alert("El dato ingresado es " + dato);
 }
 
 
