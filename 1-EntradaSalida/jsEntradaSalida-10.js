2f4391616c95808f7a187d7e9f2454355bb91df3
vidsar17 <vidsar17@gmail.com>
2020-02-05 00:11:16 -0300

    Ejercicio 9,10 de javascript

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var importe;
+    const descuento = 0.25;
+    var resultado;
+    
+    importe = document.getElementById("importe").value;
+    
+    resultado = parseInt(importe) * descuento;
+
+    document.getElementById("resultado").value = resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-  
+  var sueldo;
+  var total;
+  const aumento = 0.10;
+
+  sueldo = document.getElementById("sueldo").valor;
+
+  total = parseInt(sueldo) + parseInt(aumento);
+
+  document.getElementById("resultado").valor = total;
 	
 }
