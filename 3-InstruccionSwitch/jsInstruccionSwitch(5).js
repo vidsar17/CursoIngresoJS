20bec601d5d89a24c567cec2ab40ddb558bca9d6
Daniel Sanchez <vidsar17@yahoo.com>
2020-02-06 10:33:06 -0300

    Ejercicios SWITHC javascript

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,47 @@
 function mostrar()
-{
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
+{/* Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.". */ 
+var laHora;
 
-//alert (laHora);
+laHora = document.getElementById("hora").value;
 	
-	
+    switch(laHora)
+    {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            alert("Es de madrugada");
+        break;
 
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            alert("Es de mañana");
+        break;
 
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+            alert("Es de tarde");
+        break;
 
+        default:
+            alert("Esa hora no existe ingrese una hora entre 0 y 24");
+    }
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -1,8 +1,54 @@
 function mostrar()
-{
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
+{/* Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.".
+si está entre las 12 y las 19 : "Es de tarde.".
+si está entre las 20 y las 24 o entre las 0 y las 6 : "Es de noche.".
+si NO está entre las 0 y las 24 : "la hora no existe." */
+var laHora;
+laHora = document.getElementById('hora').value;
 
+switch(laHora)
+    {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            alert("Es de madrugada");
+        break;
 
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            alert("Es de mañana");
+        break;
 
-}//FIN DE LA FUNCIÓN
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+            alert("Es de tarde");
+        break;
+
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+        case 24:
+            alert("Es de noche");
+        break;
+
+        default:
+            alert("La hora no existe");
+    }
+    
+
+}
