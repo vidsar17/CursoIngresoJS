509eb33a0319eb29c4fb9afec2c8bddeb163391b
Daniel Sanchez <vidsar17@yahoo.com>
2020-02-06 17:54:59 -0300

    Ejercicios WHILE Javascript

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,24 +1,23 @@
 function mostrar()
 {/* Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio */
 
-	var num;
 	var i;
 	var contador = o;
 	var acumulador = 0;
 
 	alert("Ingrese 5 números \n");
 
-	i = 1;
-	while(i <= 3)
+	i = 1;		
+	while(contador <= 3)
 	{		
-		num = prompt("Ingrese el número " + (i))		
-		
-		contador = contador + 1;		
+		contador = prompt("Ingrese el número " + (i));			
+		contador = contador + 1;	
+
 		document.getElementById('suma').value = contador;
 
-		//i++;
+		// i++;
 	}
 
-		acumulador = acumulador + contador;
-		document.getElementById('promedio').value = acumulador / 3;
+		// acumulador = acumulador + contador;
+		// document.getElementById('promedio').value = acumulador / 3;
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,22 @@
 function mostrar()
-{
+{/* Al presionar el botón pedir números hasta que el USUARIO QUIERA 
+	e informar la suma acumulada y el promedio */
 
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
 
+do
+{		
+	
+	respuesta = prompt("Si desea salir del sistema ingrese SI");
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
+
+}while(respuesta);
+
+
 
 }//FIN DE LA FUNCIÓN
