259f70a343b50ade8f6d8f77f43b7abc40ad921c
Daniel Sanchez <vidsar17@yahoo.com>
2020-02-07 16:52:16 -0300

    Ejercicios WHILE Javascript

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -2,22 +2,35 @@ function mostrar()
 {/* Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio */
 
 	var i;
-	var contador = o;
-	var acumulador = 0;
+	var contador;
+	var acumulador;
+	var promedio;
+	var sum_acumulada;
+
+	contador = 0;
+	acumulador = 0;
+	promedio = 0;
+	
+
 
 	alert("Ingrese 5 números \n");
 
 	i = 1;		
-	while(contador <= 3)
+	while(i <= 3)
 	{		
-		contador = prompt("Ingrese el número " + (i));			
-		contador = contador + 1;	
 
-		document.getElementById('suma').value = contador;
+		numero = prompt("Ingrese el número " + (i));
+		numero++;		
 
-		// i++;
+		// acumulador = acumulador + numero;
+		// promedio = acumulador / contador;
+
+		
+		document.getElementById('promedio').value = numero	;
+
+		//document.getElementById('suma').value = acumulador;
+
 	}
 
-		// acumulador = acumulador + contador;
-		// document.getElementById('promedio').value = acumulador / 3;
+		
 }//FIN DE LA FUNCIÓN
