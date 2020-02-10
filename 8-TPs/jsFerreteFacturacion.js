65aa280512fa0a1444ba32619dbd8a3c7a66a27f
alumno <alumno@utnfra.com.ar>
2020-02-10 19:49:32 -0300

    Ejercicio1 tp Javascript

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,53 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var precioUno;
+var precioDos;
+var precioTres;
+var resultado;
+var iva;
+
+iva = 1.21;
+
 function Sumar () 
 {
-	
+	precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+    
+    resultado = (precioUno + precioDos + precioTres);
+    console.log(resultado);
 }
+
 function Promedio () 
 {
-	
+	precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+    
+    resultado = (precioUno + precioDos + precioTres) / 3;
+    console.log(resultado);
 }
+
 function PrecioFinal () 
 {
-	
+	precioUno = document.getElementById("PrecioUno").value;
+    precioDos = document.getElementById("PrecioDos").value;
+    precioTres = document.getElementById("PrecioTres").value;
+
+    precioUno = parseInt(precioUno);
+    precioDos = parseInt(precioDos);
+    precioTres = parseInt(precioTres);
+
+    var suma = (precioUno + precioDos + precioTres) * iva;   
+    resultado = suma + iva;    
+    console.log(resultado);
 }
