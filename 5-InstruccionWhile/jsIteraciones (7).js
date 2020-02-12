b4c17303f351530405fb2da5a8594f7975d0245f
Daniel Sanchez <vidsar17@yahoo.com>
2020-02-12 17:55:05 -0300

    "Ejercicios WHILE / DO WHILE Javascript"

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,18 +5,31 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	var promedio;
+	var i = 1;
 
 do
-{		
+{
+	i++;
+	numero = prompt("Ingrese un número");
+	respuesta = prompt("Si desea salir del sistema ingrese SI").toLowerCase();
+	numero = parseInt(numero);
+
+	contador += numero;
 	
-	respuesta = prompt("Si desea salir del sistema ingrese SI");
+	console.log("contador: " + contador);	
+	
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador/contador;
 
+	
+	console.log("variable i: " + i);
 
-	document.getElementById('suma').value=acumulador;
-	document.getElementById('promedio').value=acumulador/contador;
+}while(respuesta != "si");
 
-}while(respuesta);
-
-
+	promedio = contador / i;
+	document.getElementById('suma').value = contador;
+	document.getElementById('promedio').value = promedio;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,5 +1,6 @@
 function mostrar()
-{
+{/* Al presionar el botón pedir números hasta que el usuario quiera, 
+	sumar los que son positivos y multiplicar los negativos. */
 
 	var contador=0;
 	var positivo=0;
