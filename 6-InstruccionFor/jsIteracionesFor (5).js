99dee076964653ee91ac5e2a3dfb780f75238473
vidsar17@yahoo.com <vidsar17@yahoo.com>
2020-02-25 01:47:08 -0300

    Ej FOR Javascript

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,12 @@
 function mostrar()
-{
+{/**al presionar el botón repetir el pedido de número hasta que ingresemos el 9. */
 
-
-
-
+    for (let index = 0; index = 1; index++) {
+        repetir = parseInt(prompt("Ingrese un número ó 9 para finalizar"));
+        if(repetir == 9)
+        {
+            alert("Ha ingresado 9, gracias!!!");
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,5 +1,21 @@
 function mostrar()
-{
+{/**al presionar el botón pedir un número. mostrar los numeros pares desde el 1 al número ingresado, y mostrar la cantidad de numeros pares encontrados. */
+
+    var repetir;
+    var numero;
+    var cantPares = 0;
+    
+    repetir = parseInt(prompt("Ingrese la cantidad de números"));
+
+    for (let i = 1; i <= repetir; i++) {
+        numero = parseInt(prompt("Dígite un número"));
+        if(numero % 2 == 0)
+        {
+            cantPares++;
+        }
+        document.write("Los números ingresados fueron ", i + "<br>");
+        document.write("La cantidad de número pares ingresados es: ". cantPares + "<br>");
+    }
 
 
 
