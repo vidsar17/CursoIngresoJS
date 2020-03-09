180ba6ad21a25978f8946e7242ba9060f27b63ca
Daniel Sanchez <vidsar17@yahoo.com>
2020-03-09 20:33:04 -0300

    Javascript TP ejercicio 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,11 +1,11 @@
 function mostrar()
 {/* Ejercicio:
     Ingresar nombre, edad y pasaje('Urbano', 'Nacional', 'Internacional') y mostrar
-    a) nombre del hombre con pasaje nacional menor
-    b) el sexo con el pasajero más viejo
+    a) nombre del hombre con pasaje nacional más joven
+    b) el sexo y pasaje del pasajero más viejo
     c) cantidad de mujeres con pasaje urbano o nacional
     d) promedio edad de la mujeres
-    e)promedio de hombre con pasaje nacional
+    e) promedio de hombre con pasaje nacional
     */
 
     var respuesta;
@@ -14,7 +14,6 @@ function mostrar()
     var pasaje;
     var sexo;
     var primeraVezHombreNacional = true;
-    var primeraVezHombre = true;
     var edadMinimaHombreNacional = 0;
     var nombreMinimoNacional;
     var pasajeMaximo;
@@ -24,7 +23,7 @@ function mostrar()
     var cantidadMujeresUrbanoONacional = 0;
     var acumuladorMujeres = 0;
     var contadorMujeres = 0;
-    var acumuladorHombres = 0;
+    var acumuladorHombresNacional = 0;
     var contadorHombresPasajeNacional = 0;
     var promedioHombres;
     var promedioMujeres;
@@ -33,7 +32,7 @@ function mostrar()
       //valida nombre
       do{    
         nombre = prompt("Ingrese el nombre de la persona");
-      }while(nombre == null || nombre =="");
+      }while(nombre == null || nombre == "");
       console.log("nombre: " + nombre);
 
       //valida edad
@@ -49,15 +48,15 @@ function mostrar()
       console.log("sexo: " + sexo); 
 
       do{
-        pasaje = prompt("Indique el tipo de pasaje: \n 'Urbano', 'Nacional', 'Internacional'").toLowerCase();
+        pasaje = prompt("Indique el tipo de pasaje: \n - 'Urbano', \n - 'Nacional', \n - 'Internacional'").toLowerCase();
       }while(pasaje != 'urbano' && pasaje != 'nacional' && pasaje != 'internacional');
       console.log("El pasaje: " + pasaje);
 
-      // //a) nombre del hombre con pasaje nacional menor
+      // //a) nombre del hombre con pasaje nacional mas joven
       if(sexo == "m" && pasaje == "Nacional")
       {
-         primeraVezHombreNacional = true;
-          if(primeraVezHombre = false)
+        primeraVezHombreNacional = true;
+          if(primeraVezHombreNacional = false)
           {
             edadMinimaHombreNacional = edad;
             nombreMinimoNacional = nombre;
@@ -68,15 +67,21 @@ function mostrar()
           }
         }           
 
-        // b) el sexo con el pasajero más viejo
+        // b) el sexo y pasaje del pasajero más viejo
         if(primeraVezMaximo)
-        {primeraVezMaximo = false;
-          if(edad > edadMaxima)
+        {
+          if(primeraVezMaximo = false)
           {
             edadMaxima = edad;
+            pasajeMaximo = pasaje;
             sexoMaximo = sexo;
           }
-
+          else if(edad > edadMaxima)
+          {
+            edadMaxima = edad;
+            pasajeMaximo = pasaje;
+            sexoMaximo = sexo;
+          }
         }
     
        // c) cantidad de mujeres con pasaje urbano o nacional
@@ -86,20 +91,42 @@ function mostrar()
         }
 
         //d) promedio edad de la mujeres
-        if(sexo = 'f')
+        if(sexo == 'f')
         {
-          contadorMujeres++;          
+          acumuladorMujeres += edad;
+          contadorMujeres++;         
         }
 
-        //e)promedio de hombre con pasaje nacional
+        //e) promedio de hombre con pasaje nacional
         if(sexo == 'h' && pasaje == 'nacional')
         {
-          acumuladorHombres += edad;
+          acumuladorHombresNacional += edad;
           contadorHombresPasajeNacional++;
         }
 
   }while(respuesta = confirm("Desea continuar?"));
 
-        console.log("1ra vez edad min: " + edadMinimaHombreNacional);
-        console.log("1ra vez nombre min: " + nombreMinimoNacional);
-}
+      if(contadorMujeres > 0)
+      {
+        promedioMujeres = acumuladorMujeres / contadorMujeres;
+      }
+      else
+      {
+        promedioMujeres = 0;
+      }
+
+      if(contadorHombresPasajeNacional > 0)
+      {
+        promedioHombres = acumuladorHombresNacional / contadorHombresPasajeNacional;
+      }
+      else
+      {
+        promedioHombres = 0;
+      }
+
+      document.write("Nombre del hombre con pasaje nacional más joven es: "+ nombreMinimoNacional + "<br>");
+      document.write("El sexo " + sexoMaximo +" tiene el pasaje más viejo y es "+ pasajeMaximo + "<br>");
+      document.write("cantidad de mujeres con pasaje urbano o nacional " + cantidadMujeresUrbanoONacional + "<br>");
+      document.write("promedio edad de la mujeres "+ promedioMujeres + "<br>");
+      document.write("promedio edad de la hombres con pasaje nacional es "+ promedioHombres + "<br>");
+    }
