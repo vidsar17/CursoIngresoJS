5beaa9e60714ecb60212245e97c8588242f11ecd
vidsar17@yahoo.com <vidsar17@yahoo.com>
2020-03-12 00:34:06 -0300

    Ejercicio Parcial productos

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -7,6 +7,7 @@ function mostrar()
     el precio validar entre 100 y 300, 
     la cantidad de unidades (no puede se 0 o negativo y no debe superar las 1000 unidades), 
     la marca y el fabricante. 
+   
     Se debe informar al usuario lo siguiente:
     a) Del mas caro de los barbijos, la cantidad de unidades y el fabricante
     b) Del item con más unidades, el fabricante
@@ -15,91 +16,100 @@ function mostrar()
     var productos;
     var precio;
     var cantidad;
-    var numProductos = 5;
     var marca;
     var fabricante;
-    var flag = true;
-    var cantidadJabones = 0;
-    var mayorPrecio = 0;
-    var cantidadMayor;
-    var fabricanteBarbijo;
-    var itemMasUnidadesVendidas;
-    var fabricanteMasUnidadesVendidas;
-    var mayorPrecioBarbijo;
-    var cantidadBarbijo = 0;
+    var i = 0;
+    var canProductos = 2;
+    var barbijoMasCaro;
+    var flagBarbijo = true;
+    var cantBarbijo;
+    var fabBarijo;
+    var itemMasUnidades;
+    var fabMasUnidades = 0;
+    var flagMasUnidades = true;
+    var cantMasUnidades;
+    var unidadesJabon = 0;
 
-    for(let i = 0; i < numProductos; i++)
-    {
+   while(i < canProductos)
+   {
+            //validar productos
+        do{
+            productos = prompt("Ingrese producto número " + (i+1) + ":" + " \n - barbijo \n - jabón \n - alcohol").toLowerCase();
+        }while(productos != 'barbijo' && productos != 'jabón' && productos != 'alcohol');
+        console.log("producto: " + productos);   
 
-        do{ //validación productos
-            productos = prompt("Ingrese producto: \n - barbijo \n - jabón \n - alcohol");
-            if(productos == "jabón")
-            {
-                cantidadJabones++;                 
-            }
-
-        }while(productos != "barbijo" && productos != "jabón" && productos != "alcohol");
-
-        do{//validación precio
-            precio = parseFloat(prompt("Ingrese precio de " + productos + " entre 100 y 300"));
-
+            //validar precio
+        do{
+            precio = parseFloat(prompt("Ingrese el precio del " + productos + " entre 100 y 300: "));
         }while(isNaN(precio) || precio < 100 || precio > 300);
+        console.log("precio: " + precio);
 
-        do{//validación cantidad
-            cantidad = parseInt(prompt("Ingrese la cantidad de " + productos +" entre 1 a 1000 "));
+            //validar cantidad
+        do{
+            cantidad = parseInt(prompt("Ingrese cantidad del " + productos + " entre 1 y 1000:"));               
+        }while(isNaN(cantidad) || cantidad == "null" || cantidad <= 0 || cantidad >1000);
+        console.log("cantidad " + cantidad);
 
-        }while(cantidad <= 0 || isNaN(cantidad) || cantidad > 1000);
+            //validar marca
+        do{
+            marca = prompt("Ingrese la marca del " + productos + ": ").toLowerCase();
+        }while(marca == "");
+        console.log("marca: " + marca);
+            //validar fabricante
+        do{
+            fabricante = prompt("Ingrese el fabricante del " + productos + ": ");
+        }while(fabricante == "");
+        console.log("fabricante: " + fabricante);
 
-        do{//validación marca
-            marca = prompt("Ingrese la marca del " + productos);
-            
-        }while(marca == "" ); //isNaN(marca));
+         //a) Del mas caro de los barbijos, la cantidad de unidades y el fabricante
+         if(productos == "barbijo")
+         {
+             if(flagBarbijo)
+             {
+                 flagBarbijo = false;
+                 barbijoMasCaro = precio;
+                 fabBarijo = fabricante;
+                 cantBarbijo = cantidad;
+             }else if(precio > barbijoMasCaro)
+             {
+                 barbijoMasCaro = precio;
+                 fabBarijo = fabricante;
+                 cantBarbijo = cantidad;
+             }            
+         }
 
-        do{//validación fabricante
-            fabricante = prompt("Ingrese la fabricante del " + productos);
-            
-        }while(fabricante == "" ); //|| isNaN(fabricante));
-       
-    } 
-            //b) Del item con más unidades, el fabricante
-            if(flag)
-            {flag = false;
-                mayorPrecio = precio;
-                itemMasUnidadesVendidas = productos;
-                fabricanteMasUnidadesVendidas = fabricante;
-                cantidadMayor = cantidad;
-            }else if(precio > mayorPrecio)
+         // b) Del item con más unidades, el fabricante
+            if(flagMasUnidades)
             {
-                mayorPrecio = precio;
-                itemMasUnidadesVendidas = productos;
-                fabricanteMasUnidadesVendidas = fabricante;
-                cantidadMayor = cantidad;
+                flagMasUnidades = false;
+                itemMasUnidades = productos;
+                fabMasUnidades = fabricante;
+                cantMasUnidades = cantidad;
+            }else if(cantidad > cantMasUnidades)
+            {
+                itemMasUnidades = productos;
+                fabMasUnidades = fabricante;
+                cantMasUnidades = cantidad;
             }
-    
-          //a) Del mas caro de los barbijos, la cantidad de unidades y el fabricante
-          if(productos == "barbijo")
-          {                 
-              mayorPrecioBarbijo = mayorPrecio;
-              fabricanteBarbijo = fabricanteMasUnidadesVendidas;
-              cantidadBarbijo = cantidadMayor;
-              document.write("Barbijo caro es de: " + mayorPrecioBarbijo + ", la cantidad de unidades es: " + cantidadBarbijo + 
-              " y el fabricante es: " + fabricanteBarbijo +"<br>");
-          }else
-          {
-                     document.write("No se ingresaron barbijos" + "<br>");
-          }       
-  
-          //C) Cuantas unidades de jabones hay en total
-          if(productos == "jabón")
-          {
-            document.write("Cantidad de jabones es de: " + cantidadJabones +"<br>");       
-          }else
-          {
-              document.write("No se ingresaron jabones " + "<br>");
-          }
-              
 
-        document.write("El ítem con más unidades vendidas es: " + itemMasUnidadesVendidas +
-         " y su fabricante es: " + fabricanteMasUnidadesVendidas +"<br>");
-        
+            //C) Cuantas unidades de jabones hay en total
+            if(productos == 'jabón')
+            {
+                unidadesJabon++;
+            }
+
+    i++;
+    }
+        if(productos == "barbijo")
+        {
+            document.write("El barbijo más caro es de: $"+ barbijoMasCaro + ", el fabricante es " + fabBarijo + " y la cantidad es de " + cantBarbijo + "<br>");
+        }else
+        {
+            document.write("No se ingresaron barbijos" + "<br>");
+        }
+
+        document.write("El ítem con más unidades vendidas es: " + itemMasUnidades + ", el fabricante es: " + fabMasUnidades + " y la cantidad de unidades vendidas es: " + cantMasUnidades + "<br>");
+
+        document.write("La cantidad de jabones ingresado es de: " + unidadesJabon + "<br>");
+
 }
